42760f682ee579dcec39e6b9f872f94352f161b5
M B Parvez (Ron) <mbparvezme@gmail.com>
2024-12-17 11:09:50 +0600

    Removed old docs from new branch

--- src/lib/index.ts
+++
@@ -1,4 +0,0 @@
-
-export const randomString = (prefix: string = "block") => `${prefix}${Array.from({ length: 5 }, () => String.fromCharCode(97 + Math.floor(Math.random() * 26))).join('') + Date.now()}`;
-
-export const processID = (title: string, hash: boolean = true) => `${hash ? '#' : ''}${title.toLowerCase().replace(/\s+/g, '-')}`
