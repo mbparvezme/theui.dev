7cb06a65b8224ca3111b65519b30f324ce822ec6
M B Parvez (Ron) <mbparvezme@gmail.com>
2024-12-31 13:53:57 +0600

    Utility page started

--- src/lib/ui/doc/Example.svelte
+++ src/lib/ui/doc/Example.svelte
@@ -1,36 +1,40 @@
 <script lang="ts">
   import type { Snippet } from "svelte";
   import Code from "./CodeBlock.svelte";
+    import { twMerge } from "tailwind-merge";
 
   interface Props {
     children?: Snippet,
     example?: Snippet,
     code?: Snippet,
+    classes?: string,
     title?: string,
     codeTitle?: string,
   };
 
-  let {children, example, code, title, codeTitle = ""}: Props = $props();
+  let {children, example, code, classes, title, codeTitle}: Props = $props();
 </script>
 
-{#if title}
-  <h4 class="font-semibold font-body text-gray-500 mb-0 mt-4">{@html title}</h4>
-{/if}
+<div>
+  {#if title}
+    <h4 class="font-semibold font-body text-muted mt-0 mb-1">{@html title}</h4>
+  {/if}
 
-{@render children?.()}
+  {@render children?.()}
 
-<div class="rounded-md bg-secondary dark:bg-black/50">
+  <div class="rounded-md bg-secondary dark:bg-black/50 mt-4">
 
-  {#if example}
-  <div class="p-4 not-prose">
-    {@render example()}
+    {#if example}
+    <div class="not-prose text-center { twMerge("p-4", classes)}">
+      {@render example()}
+    </div>
+    {/if}
+
+    {#if code}
+    <Code space="none" title={codeTitle}>
+      {@render code()}
+    </Code>
+    {/if}
+
   </div>
-  {/if}
-
-  {#if code}
-  <Code space="none" title={codeTitle}>
-    {@render code()}
-  </Code>
-  {/if}
-
 </div>
--- src/routes/test/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-import Content from "$lib/test/test.svx";
-</script>
-
-
-<div class="py-96">
-  <Content />
-</div>
