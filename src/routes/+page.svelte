fccda88c776352aa2809ffe4b92d15476dcb9622
M B Parvez (Ron) <mbparvezme@gmail.com>
2025-01-15 16:50:44 +0600

    Dropdown completed, Modal started

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,8 +6,16 @@
 <p class="bg-yellow-400 text-black"></p>
 <p class="text-pink-500"></p>
 <p class="text-green-500 w-96 border-2 border-green-500 flex-wrap gap-y-4"></p>
+<p class="text-green-500 w-96 border-2 border-green-500 flex-wrap gap-y-4"
+  containerClasses="border-4 rounded-lg"
+  linkClasses="text-green-500 hover:bg-green-100"
+  activeLinkClasses="text-white bg-green-500"
+  dividerClasses="border-b-2 border-green-200"
+  headerClasses="text-green-600 text-xs"
+  dropdownClasses="shadow-lg bg-green-50"
+></p>
 <p class="text-green-50 bg-green-100 content-center border-brand-primary-500"></p>
 
-<div class="bg-gradient-to-r from-red-400 to-yellow-500">
+<div class="bg-gradient-to-r from-red-400 to-yellow-500 bg-gray-500/50 grow-0">
 
 </div>
