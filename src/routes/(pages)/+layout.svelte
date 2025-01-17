cc776ce89f7455ea9b882be4fa9522668c1bc78c
M B Parvez (Ron) <mbparvezme@gmail.com>
2025-01-17 16:57:10 +0600

    Modal going on

--- src/routes/(pages)/+layout.svelte
+++ src/routes/(pages)/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { type Snippet } from "svelte";
-	import '../app.css';
+	import '../../app.css';
 	import Navbar from  "$lib/ui/Navbar.svelte";
 	import Footer from  "$lib/ui/Footer.svelte";
 	import {Tooltip} from "theui-svelte"
---
+++ src/routes/example/modal-open-state/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { Button, Container, Modal } from "theui-svelte";
+</script>
+
+<Container>
+  <Modal modalStatus={true}>
+    Hello Modal!
+  </Modal>
+</Container>
