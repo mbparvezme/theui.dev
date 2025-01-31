69e4425902679ea7f259b833704277055fd9acd2
mbparvezme <mbparvezme@gmail.com>
2025-02-01 01:08:10 +0600

    Working on notification

--- src/routes/example/notification-variant/+page.svelte
+++ src/routes/example/notification-variant/+page.svelte
@@ -8,17 +8,17 @@
   <div class="mt-auto">
     <p class="font-semibold">Default theme</p>
     <ButtonGroup  size="sm">
-      <Button onclick={()=>notify("Hello world!", "warning", {variant: "card"})}>Card (Default)</Button>
-      <Button onclick={()=>notify("Hello world!", "error", {variant: "borderBottom"})}>Border Bottom</Button>
-      <Button onclick={()=>notify("Hello world!", "info", {variant: "borderStart"})}>Border Start</Button>
+      <Button onclick={()=>notify("Hello world!", "success", {variant: "card"})}>Card (Default)</Button>
       <Button onclick={()=>notify("Hello world!", "success", {variant: "borderTop"})}>Border Top</Button>
+      <Button onclick={()=>notify("Hello world!", "success", {variant: "borderStart"})}>Border Start</Button>
+      <Button onclick={()=>notify("Hello world!", "success", {variant: "borderBottom"})}>Border Bottom</Button>
     </ButtonGroup>
     <p class="font-semibold mt-4">Light theme</p>
     <ButtonGroup size="sm">
-      <Button onclick={()=>notify("Hello world!", "warning", {theme: "light", variant: "card"})}>Card (Default)</Button>
-      <Button onclick={()=>notify("Hello world!", "error", {theme: "light", variant: "borderBottom"})}>Border Bottom</Button>
-      <Button onclick={()=>notify("Hello world!", "info", {theme: "light", variant: "borderStart"})}>Border Start</Button>
+      <Button onclick={()=>notify("Hello world!", "success", {theme: "light", variant: "card"})}>Card (Default)</Button>
       <Button onclick={()=>notify("Hello world!", "success", {theme: "light", variant: "borderTop"})}>Border Top</Button>
+      <Button onclick={()=>notify("Hello world!", "success", {theme: "light", variant: "borderStart"})}>Border Start</Button>
+      <Button onclick={()=>notify("Hello world!", "success", {theme: "light", variant: "borderBottom"})}>Border Bottom</Button>
     </ButtonGroup>
   </div>
 </Container>
