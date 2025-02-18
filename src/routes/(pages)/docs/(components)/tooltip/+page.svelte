00311882c2c691b4358309e278852150bd94f438
M B Parvez (Ron) <mbparvezme@gmail.com>
2025-02-18 16:40:47 +0600

    Working on Tooltip

--- src/routes/(pages)/docs/(components)/tooltip/+page.svelte
+++ src/routes/(pages)/docs/(components)/tooltip/+page.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
   import type { PageData } from "./$types";
   import Page from "$lib/pages/tooltip.svx";
+  import { onMount } from "svelte";
 
   let { data }: { data: PageData|any } = $props();
+
+  onMount(() => {
+    const getEBound = (el: HTMLElement): DOMRect => el.getBoundingClientRect();
+    console.log(getEBound.top);
+  });
 </script>
 
 <Page component={data.component} edit_url={data.editURL} />
