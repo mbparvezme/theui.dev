f06bdaab8143f624b72526e71b1fa892dcaa9ce1
M B Parvez (Ron) <mbparvezme@gmail.com>
2025-03-14 12:04:06 +0600

    Cloudflare adapter issue resolved

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import { mdsvex } from "mdsvex";
-// import adapter from "@sveltejs/adapter-cloudflare";
-import adapter from '@sveltejs/adapter-cloudflare-workers';
+import adapter from "@sveltejs/adapter-cloudflare";
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
