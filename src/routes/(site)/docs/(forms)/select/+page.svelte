505a54870b04f4611624c6fd98e8b9a27b398c4e
mbparvezme <mbparvezme@gmail.com>
2025-03-24 02:32:57 +0600

    Working with Select

--- src/routes/(site)/docs/(forms)/select/+page.svelte
+++ src/routes/(site)/docs/(forms)/select/+page.svelte
@@ -1,8 +1,11 @@
-<script>
-  import ComingSoon from "$lib/ui/ComingSoon.svelte";
+<script lang="ts">
+  import type { PageData } from "./$types";
+  import Page from "$lib/pages/select.svx";
+
+  let { data }: { data: PageData|any } = $props();
   import SEO from "$lib/SEO.svelte"
 </script>
 
-<SEO title="Select Input" />
+<SEO title="Select" />
 
-<ComingSoon class="h-auto" />
+<Page component={data.component} edit_url={data.editURL} />
