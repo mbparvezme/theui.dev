1b17ab9822ead26a3e38f3daf569d3b9d487121e
mbparvezme <mbparvezme@gmail.com>
2025-03-24 06:06:08 +0600

    Select component started

--- src/lib/ui/doc/Example.svelte
+++ src/lib/ui/doc/Example.svelte
@@ -8,13 +8,17 @@
     children?: Snippet,
     example?: Snippet,
     code?: Snippet,
+    children2?: Snippet,
+    example2?: Snippet,
+    code2?: Snippet,
     classes?: string,
     title?: string,
     codeTitle?: string,
+    code2Title?: string,
     mt?: boolean
   };
 
-  let {children, example, code, classes, title, codeTitle, mt = true}: Props = $props();
+  let {children, children2, example, example2, code, code2, classes, title, codeTitle, code2Title, mt = true}: Props = $props();
 </script>
 
 <div class="example-section w-full" class:!mt-0={!mt}>
@@ -36,6 +40,25 @@
       {#if code}
       <Code classes="border-0" space="none" title={codeTitle}>
         {@render code()}
+      </Code>
+      {/if}
+
+    </div>
+  {/if}
+
+  {@render children2?.()}
+  {#if example2 || code2}
+    <div class="rounded-md mt-4 border border-gray-200 dark:border-gray-800">
+
+      {#if example2}
+      <div class="not-prose {twMerge("p-5 flex justify-center items-center gap-4 rounded-md bg-white dark:bg-black/40", classes)}">
+        {@render example2()}
+      </div>
+      {/if}
+
+      {#if code2}
+      <Code classes="border-0" space="none" title={codeTitle}>
+        {@render code2()}
       </Code>
       {/if}
 
