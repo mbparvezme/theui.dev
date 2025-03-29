2eaedddf07214470cd0efc7d86baf34bbb2eee29
mbparvezme <mbparvezme@gmail.com>
2025-03-30 05:38:38 +0600

    Workin on Navbar, Tooltip completed

--- src/routes/example/navbar/+page.svelte
+++ src/routes/example/navbar/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
+  import type { PageData } from "./$types"
+  import type {NAV_SCROLL_BEHAVIOR } from "theui-svelte/type"
   import {Navbar, NavBrand, NavLinkGroup, NavDropdown, NavLink} from "theui-svelte"
+  export let data:PageData
+  const values = ['default', 'fixed', 'shrinkOnScrollDown', 'hideOnScrollDown', 'shrinkAndHide'] as const
 </script>
 
-<Navbar>
+<Navbar scrollBehavior={values.includes(data.navType as NAV_SCROLL_BEHAVIOR) ? data.navType as NAV_SCROLL_BEHAVIOR : "default"}>
   <NavBrand href="/">BRAND</NavBrand>
   <NavLinkGroup>
     <NavLink href="/" text="Link 1" />
@@ -15,3 +19,7 @@
     </NavDropdown>
   </NavLinkGroup>
 </Navbar>
+
+<div class="h-screen"></div>
+<div class="h-screen bg-gray-200"></div>
+<div class="h-screen"></div>
