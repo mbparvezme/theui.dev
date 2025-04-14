c4dfbc541ecd1387daf4c4ceaded4b44fd42ad1b
mbparvezme <mbparvezme@gmail.com>
2025-04-15 01:55:20 +0600

    UI Blocks JSON added

--- src/lib/ui/PageHeader.svelte
+++ src/lib/ui/PageHeader.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type { Snippet } from 'svelte';
+    import { twMerge } from 'tailwind-merge';
   import { Container, Button } from 'theui-svelte'
 
   interface Props {
@@ -10,14 +11,15 @@
     h2?: string
     text?: string
     ctaTxt?: string
-    ctaUrl?: string
+    ctaUrl?: string,
+    classes?: string,
   }
 
-  let {children, beforeItem, afterCTA, h1, h2, ctaTxt, ctaUrl }: Props = $props()
+  let {children, beforeItem, afterCTA, h1, h2, ctaTxt, ctaUrl, classes}: Props = $props()
 </script>
 
-<header class="min-h-screen">
-  <Container class="px-8 md:px-16 flex flex-col items-center gap-y-8 text-center pt-36 md:pt-48 pb-24">
+<header class="">
+  <Container class={twMerge("px-8 md:px-16 flex flex-col items-center gap-y-8 text-center pt-36 md:pt-48 pb-24", classes)}>
     {@render beforeItem?.()}
     {#if h1}<h1 class="text-4xl sm:text-5xl lg:text-6xl font-black font-display">{@html h1}</h1>{/if}
     {#if h2}<h2 class="text-2xl md:text-4xl xl:text-4xl text-muted font-display">{@html h2}</h2>{/if}
--- src/routes/(site)/ui-blocks/+page.svelte
+++ src/routes/(site)/ui-blocks/+page.svelte
@@ -1,11 +1,33 @@
 <script lang="ts">
+  import type { PageData } from "../$types"
+  import { Button, Card, Container } from 'theui-svelte'
   import PageHead from "$lib/ui/PageHeader.svelte"
   import SEO from "$lib/SEO.svelte"
-  import ComingSoon from "$lib/ui/ComingSoon.svelte";
+  import ComingSoon from "$lib/ui/ComingSoon.svelte"
+  let { data }: {data: PageData } = $props()
 </script>
 
 <SEO title="UI Blocks" />
 
 <ComingSoon title="Exciting UI blocks" />
 
-<!-- <PageHead h1 = "UI Blocks <br class='sm:hidden md:block'>That You NeedRight Now" h2 = "Coming Soon!" /> -->
+<!-- <PageHead h1 = "Supercharge Your UI Workflow <br class='sm:hidden md:block'>with Ready UI Blocks" h2="Build beautiful apps in minutes, not hours." classes="text-start items-start">
+  {#snippet afterCTA()}
+    <p class="w-1/2">Drop-in UI blocks built specifically for Svelte and Tailwind — fast to implement, cleanly designed, and fully reusable. Skip the hassle of designing from scratch. Simply copy the block you need, paste it into your project, and focus on building great functionality instead of wasting time on layouts.</p>
+    <div>
+      <Button href="#" rounded="full" size="lg" class="me-4">Get Free Blocks</Button>
+      <Button href="#" rounded="full" size="lg" outline>Explore Pricing</Button>
+    </div>
+  {/snippet}
+</PageHead>
+
+<Container>
+  <h1 class="text-3xl font-bold text-center mb-8">THEUI UI BLOCKS</h1>
+  <section class="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-8">
+    {#each data.blockCategories as category}
+      <Card class="dark:bg-secondary" title={category.title}>
+        <a class="absolute inset-0" href={"ui-blocks/"+category.slug} aria-label={category.title + " link"}>&nbsp;</a>
+      </Card>
+    {/each}
+  </section>
+</Container> -->
