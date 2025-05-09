4bb400cfba4ab3a791c665c075f0e49cb0fbb30b
M B Parvez (Ron) <mbparvezme@gmail.com>
2025-05-09 16:27:34 +0600

    Working on Doc fix

--- src/routes/(site)/docs/+layout.svelte
+++ src/routes/(site)/docs/+layout.svelte
@@ -4,13 +4,8 @@
   import { Container, Svg, Drawer } from "theui-svelte";
   import { twMerge } from 'tailwind-merge';
 
-  /**
-   * 
-  */
-
-
   let { children, data }: {children: Snippet, data: any } = $props();
-  let linkClasses = (active: boolean = false) => twMerge("border-s-2 border-gray-500/10 ps-4 py-1 inline text-default", active && "border-brand-primary-500 text-brand-primary-500");
+  let linkClasses = (active: boolean = false) => twMerge("border-s-2 border-gray-500/10 ps-4 py-1 inline text-default tracking-wide font-normal", active && "border-brand-primary-500 text-brand-primary-500 dark:border-brand-primary-200 dark:text-brand-primary-200");
 </script>
 
 <div class="fixed right-8 bottom-8 lg:hidden">
